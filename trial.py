d8d3b517b92b4a4d3502ecdd61f5587f01a26e3e
seftywandira <sefty.wandira@gmail.com>
2025-10-01 15:12:15 +0800

    oke

---
+++ trial.py
@@ -0,0 +1,11 @@
+#
+# trial, 2025/10/01
+# File: trial.py
+# Short description of the task
+#
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
---
+++ trial2.py
@@ -0,0 +1,11 @@
+#
+# trial2, 2025/10/01
+# File: trial2.py
+# Short description of the task
+#
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
