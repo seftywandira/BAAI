694706321f0d272e06576cca809e2da5a2167efa
seftywandira <sefty.wandira@gmail.com>
2025-10-22 15:59:19 +0800

    g

--- Sefty_Correlation_Analysis.py
+++ Sefty_Correlation_Analysis.py
@@ -5,7 +5,15 @@
 #
 
 # 1. Input
+import pandas as pd
+import numpy as np 
+
+df = pd.read_csv('Simple_Data.csv')
+# df = pd.read_csv('Correlataion_Analysis_Data.csv')
 
 # 2. Process
+print(df.isnull().sum)
 
 # 3. Output
+print('Data loaded successfully!')
+print(f'Datset shape : {df.shape}')
