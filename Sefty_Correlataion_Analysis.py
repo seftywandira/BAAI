33a29743e6d7b24f15b48345c336884f07751db5
seftywandira <sefty.wandira@gmail.com>
2025-10-23 18:17:22 +0800

    rehat

---
+++ Sefty_Correlataion_Analysis.py
@@ -0,0 +1,18 @@
+#
+# Sefty, 2025/10/22
+# File: Sefty_Correlation_Analysis.py
+# Apply correlation analysis to business problems
+#
+
+import pandas as pd
+
+# 1. Input
+# Read the CSV file
+df = pd.read_csv('Correlataion_Analysis_Data.csv')
+
+# 2. Process
+
+# 3. Output
+print('Data loaded successfully!')
+print(f'Datset shape : {df.shape}')
+
