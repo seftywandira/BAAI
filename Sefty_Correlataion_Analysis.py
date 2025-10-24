c1597e08d5f381e53bae658339b5e159ee7f4e05
seftywandira <sefty.wandira@gmail.com>
2025-10-24 08:53:21 +0800

    pagi

--- Sefty_Correlataion_Analysis.py
+++ Sefty_Correlataion_Analysis.py
@@ -4,15 +4,31 @@
 # Apply correlation analysis to business problems
 #
 
+# Data Manipulation and Analysis
 import pandas as pd
+import numpy as np
+
+# Data Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Statistical Analysis
+from scipy import stats
 
 # 1. Input
 # Read the CSV file
 df = pd.read_csv('Correlataion_Analysis_Data.csv')
+df.info()
 
 # 2. Process
+correlation_matrix = df.iloc[:,1:6].corr()
+print(correlation_matrix.round(3))
 
 # 3. Output
-print('Data loaded successfully!')
-print(f'Datset shape : {df.shape}')
+# print('Data loaded successfully!')
+# print(f'Datset shape : {df.shape}')
+sns.heatmap(correlation_matrix)
+plt.title('Sefty is the most intelligent person in the world')
+plt.tight_layout()
+plt.show()
 
